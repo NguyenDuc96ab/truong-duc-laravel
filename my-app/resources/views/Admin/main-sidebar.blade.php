1a844f6c2efb2cfa5f63713a5eb75f77c1d69266
NguyenDuc96a <94058422+NguyenDuc96a@users.noreply.github.com>
2023-11-30 15:10:46 +0700

    tạo bảng category

--- my-app/resources/views/Admin/main-sidebar.blade.php
+++ my-app/resources/views/Admin/main-sidebar.blade.php
@@ -1,7 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-        <img src="/dist/img/AdminLTELogo-1.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="/dist/img/AdminLTELogo-1.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
         <span class="brand-text font-weight-light">Trang quản trị</span>
     </a>
 
@@ -49,6 +50,35 @@
                         <p>Danh sách trang</p>
                     </a>
                 </li>
+                <li class="nav-header">QUẢN LÝ DANH MỤC</li>
+                <li class="nav-item">
+                    <a href="/admin/categorys/add" class="nav-link">
+                        <i class="nav-icon far fa-edit"></i>
+                        <p>Tạo mới danh mục</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="/admin/categorys/list" class="nav-link">
+                        <i class="nav-icon far fa-edit"></i>
+                        <p>Danh sách danh mục</p>
+                    </a>
+                </li>
+
+                <!-- <li class="nav-header">QUẢN LÝ MENU</li>
+                <li class="nav-item">
+                    <a href="/admin/menus/add" class="nav-link">
+                        <i class="nav-icon far fa-edit"></i>
+                        <p>Tạo mới menu</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="/admin/menus/list" class="nav-link">
+                        <i class="nav-icon far fa-edit"></i>
+                        <p>Danh sách menu</p>
+                    </a>
+                </li> -->
                 <li class="nav-header">QUẢN LÝ SẢN PHẨM</li>
                 <li class="nav-item">
                     <a href="/admin/products/add" class="nav-link">
