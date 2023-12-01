ca26a2151921162d7025c81a2a2f2536d7b626ce
NguyenDuc96a <94058422+NguyenDuc96a@users.noreply.github.com>
2023-12-01 12:00:20 +0700

    chỉnh sửa giao diện bị vỡ trang giới thiệu

--- my-app/resources/views/Web/introduce/introduce-list.blade.php
+++ my-app/resources/views/Web/introduce/introduce-list.blade.php
@@ -1,46 +1,35 @@
 @extends('Web.main')
 
 @section('content')
+<section id="content" class="clearfix container">
 
 
-<div class="container-mp nav-wrapper">
-
-    <!--End: Bài viết mới nhất-->
 
     @include('Web.sidebar')
 
-    @foreach($postObject as $post)
-    <div class="col-md-9" id="layout-page">
-        <span class="header-page clearfix">
-            <h1>{{$post->name}}</h1>
-        </span>
 
-        <div class="content-page">
+    <div>
+        @foreach($postObject as $post)
+        <div class="col-md-9" id="layout-page">
+            <span class="header-page clearfix">
+                <h1>{{$post->name}}</h1>
+            </span>
 
-            {!! $post->content !!}
+            <div class="content-page">
+
+                {!! $post->content !!}
+            </div>
         </div>
-    </div>
-    @endforeach
-
-    <div class="cs_muahang" style="clear: both;">
-
-
-
+        @endforeach
     </div>
 
+</section>
+
+
+@endsection
 
 
 
-
-
-
-
-    </section>
-
-    @endsection
-
-
-
-    @section('footer')
-    @include('Web.footer')
-    @endsection
+@section('footer')
+@include('Web.footer')
+@endsection
